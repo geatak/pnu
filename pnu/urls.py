a7257f6eb3bd8bcf7caecbf55db2c31e5a532729
geatak <kthgogook7@gmail.com>
2019-04-12 17:07:34 +0900

    ㅅㄷㄴㅅ

--- pnu/urls.py
+++ pnu/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from django.shortcuts import redirect
+def root(request):
+    return redirect("/shop/")
+
 urlpatterns = [
+    path('', root),
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
 ]
