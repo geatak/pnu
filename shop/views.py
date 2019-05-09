b97715ff28834967de71e22ac4aa60e9c678f75e
geatak <47970884+geatak@users.noreply.github.com>
2019-05-09 17:12:51 +0900

    tttt

--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,21 @@
 # from django.views.generic import ListView
-from django.shortcuts import render
+from django.shortcuts import redirect,render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Shop, Item
+from .forms import ShopForm
 
 
 def shop_list(request):
+    query = request.GET.get('query', '').strip()
     qs = Shop.objects.all()
+    if query:
+        qs = qs.filter(name__icontains=query)
     return render(request, 'shop/shop_list.html', {
         'shop_list': qs,
+        'query': query,
     })
+
+# shop_list = ListView.as_view(model=Shop, paginate_by=20)
 
 
 def shop_detail(request, pk):
@@ -15,6 +23,43 @@ def shop_detail(request, pk):
     return render(request, 'shop/shop_detail.html', {
         'shop': shop,
     })
+
+# shop_detail = DetailView.as_view(model=Shop)
+
+
+def shop_new(request):
+    if request.method == 'POST':  # 'GET', 'POST'
+        # request.GET, request.POST, request.FILES
+        form = ShopForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/shop/')
+    else:
+        form = ShopForm()
+    return render(request, 'shop/shop_form.html', {
+        'form': form,
+    })
+
+# shop_new = CreateView.as_view(model=Shop, form_class=ShopForm,
+#                               success_url='/shop/')
+
+
+def shop_edit(request, pk):
+    shop = Shop.objects.get(pk=pk)
+    if request.method == 'POST':  # 'GET', 'POST'
+        # request.GET, request.POST, request.FILES
+        form = ShopForm(request.POST, request.FILES, instance=shop)
+        if form.is_valid():
+            form.save()
+            return redirect('/shop/')
+    else:
+        form = ShopForm(instance=shop)
+    return render(request, 'shop/shop_form.html', {
+        'form': form,
+    })
+
+# shop_edit = UpdateView.as_view(model=Shop, form_class=ShopForm,
+#                                success_url='/shop/')
 
 
 def item_list(request):
